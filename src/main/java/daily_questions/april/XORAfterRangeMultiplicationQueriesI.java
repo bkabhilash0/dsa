878d12fa9d8c6d5df62f4b6ba76c8eb34530eb3b
bkabhilash0 <bkabhilash0@gmail.com>
2026-04-08 23:27:33 +0530

    Add XOR After Range Multiplication Queries implementation

---
+++ src/main/java/daily_questions/april/XORAfterRangeMultiplicationQueriesI.java
@@ -0,0 +1,51 @@
+package daily_questions.april;
+
+/**
+ * Date: April 8, 2026,
+ * You are given an integer array nums of length n and
+ * a 2D integer array queries of size q, where queries[i] = [li, ri, ki, vi].
+ * For each query, you must apply the following operations in order:
+ * Set idx = li.
+ * While idx <= ri:
+ * Update: nums[idx] = (nums[idx] * vi) % (109 + 7)
+ * Set idx += ki.
+ * Return the bitwise XOR of all elements in nums after processing all queries.
+ */
+public class XORAfterRangeMultiplicationQueriesI {
+    final int mod = 1000000007;
+
+    public int xorAfterQueries(int[] nums, int[][] queries) {
+
+        // Process each query
+        for (int[] t : queries) {
+            int l = t[0];
+            int r = t[1];
+            int k = t[2];
+            int v = t[3];
+
+            int idx = l;
+
+            // Apply operation at step k
+            while (idx <= r) {
+                long temp = nums[idx];
+                nums[idx] = (int)((temp * v) % mod);
+                idx += k;
+            }
+        }
+
+        // Compute XOR of final array
+        int ans = 0;
+        for (int num : nums) {
+            ans ^= num;
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 1};
+        int[][] queries = {{0, 2, 1, 4}};
+        XORAfterRangeMultiplicationQueriesI xorAfterRangeMultiplicationQueriesI = new XORAfterRangeMultiplicationQueriesI();
+        System.out.println(xorAfterRangeMultiplicationQueriesI.xorAfterQueries(nums, queries)); // Output: 12
+    }
+}
