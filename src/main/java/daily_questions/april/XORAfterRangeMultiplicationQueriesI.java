98c8961a3bb8b16478a36a5cb44d6b4c92f3ed0d
bkabhilash0 <bkabhilash0@gmail.com>
2026-04-09 23:40:08 +0530

    Add documentation for XOR After Range Multiplication Queries I

--- src/main/java/daily_questions/april/XORAfterRangeMultiplicationQueriesI.java
+++ src/main/java/daily_questions/april/XORAfterRangeMultiplicationQueriesI.java
@@ -2,6 +2,7 @@ package daily_questions.april;
 
 /**
  * Date: April 8, 2026,
+ * <a href="https://leetcode.com/problems/xor-after-range-multiplication-queries-i/">XOR After Range Multiplication Queries I</a>
  * You are given an integer array nums of length n and
  * a 2D integer array queries of size q, where queries[i] = [li, ri, ki, vi].
  * For each query, you must apply the following operations in order:
@@ -14,6 +15,8 @@ package daily_questions.april;
 public class XORAfterRangeMultiplicationQueriesI {
     final int mod = 1000000007;
 
+    // Time Complexity: O(q), where q is the total number of queries
+    // Space Complexity: O(1), as we are modifying the input array in place and using only a constant amount of extra space
     public int xorAfterQueries(int[] nums, int[][] queries) {
 
         // Process each query
@@ -28,7 +31,7 @@ public class XORAfterRangeMultiplicationQueriesI {
             // Apply operation at step k
             while (idx <= r) {
                 long temp = nums[idx];
-                nums[idx] = (int)((temp * v) % mod);
+                nums[idx] = (int) ((temp * v) % mod);
                 idx += k;
             }
         }
