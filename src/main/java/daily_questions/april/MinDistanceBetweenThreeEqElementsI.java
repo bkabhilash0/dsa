cb2cbbc133c0aca69e1e006d7f6f73cdc54d4005
bkabhilash0 <bkabhilash0@gmail.com>
2026-04-10 23:17:09 +0530

    Add Minimum Distance Between Three Equal Elements I implementation

---
+++ src/main/java/daily_questions/april/MinDistanceBetweenThreeEqElementsI.java
@@ -0,0 +1,81 @@
+package daily_questions.april;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Date: April 10, 2026,
+ * <a href="https://leetcode.com/problems/minimum-distance-between-three-equal-elements-i/">Minimum Distance Between Three Equal Elements I</a>
+ * You are given an integer array nums.
+ * A tuple (i, j, k) of 3 distinct indices is good if nums[i] == nums[j] == nums[k].
+ * The distance of a good tuple is abs(i - j) + abs(j - k) + abs(k - i), where abs(x) denotes the absolute value of x.
+ * Return an integer denoting the minimum possible distance of a good tuple. If no good tuples exist, return -1.
+ */
+public class MinDistanceBetweenThreeEqElementsI {
+    // Time Complexity: O(N) + O(N * k)
+    // Space Complexity: O(N)
+    private int intuition(int[] nums) {
+        if (nums.length < 3) {
+            return -1;
+        }
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.computeIfAbsent(nums[i], k -> new java.util.ArrayList<>()).add(i);
+        }
+        int i, j, k;
+        int ans = Integer.MAX_VALUE;
+        for (Map.Entry<Integer, List<Integer>> entry : map.entrySet()) {
+            List<Integer> list = entry.getValue();
+            if (list.size() < 3) {
+                continue;
+            }
+            i = 0;
+            j = 1;
+            k = 2;
+            while (k < list.size()) {
+                ans = Math.min(ans, Math.abs(list.get(i) - list.get(j)) + Math.abs(list.get(j) - list.get(k)) + Math.abs(list.get(k) - list.get(i)));
+                i++;
+                j++;
+                k++;
+            }
+        }
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+
+    // Time Complexity: O(n), where n is the length of the input array nums.
+    // Space Complexity: O(n), in the worst case, if all elements in nums are same
+    private int better(int[] nums) {
+        if (nums.length < 3) return -1;
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        int ans = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                List<Integer> list = map.get(nums[i]);
+                list.add(i);
+                map.put(nums[i], list);
+                if (list.size() > 3) {
+                    list.removeFirst();
+                }
+                if (list.size() == 3) {
+                    ans = Math.min(ans, Math.abs(list.get(0) - list.get(1)) + Math.abs(list.get(1) - list.get(2)) + Math.abs(list.get(2) - list.get(0)));
+                }
+            } else {
+                List<Integer> list = new java.util.ArrayList<>();
+                list.add(i);
+                map.put(nums[i], list);
+            }
+        }
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+
+    public int minimumDistance(int[] nums) {
+        return better(nums);
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 1, 1, 3};
+        MinDistanceBetweenThreeEqElementsI minDistanceBetweenThreeEqElementsI = new MinDistanceBetweenThreeEqElementsI();
+        System.out.println(minDistanceBetweenThreeEqElementsI.minimumDistance(nums)); // Output: 6
+    }
+}
